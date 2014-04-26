8603f36b9d698a0f2414bb1b9dbfbc58e9160d12
Roberto Allende <rallende@gmail.com>
2014-04-26 18:23:35 -0300

    Adding Django Rest Framework to shape

--- server/servicearea/urls.py
+++ server/servicearea/urls.py
@@ -1,7 +1,28 @@
 from django.conf.urls import patterns, include, url
+from shape.models import Shape
+from rest_framework import viewsets, routers
+ 
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Permission
 
 from django.contrib import admin
 admin.autodiscover()
+
+class PermissionViewSet(viewsets.ModelViewSet):
+    model = Permission
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    model = User
+    
+class ShapeViewSet(viewsets.ModelViewSet):
+    model = Shape
+
+ 
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'shape', ShapeViewSet)
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,4 +30,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )

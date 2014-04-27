80a51412027519d7833297699ab25ce43a5f1e14
Roberto Allende <rallende@gmail.com>
2014-04-27 04:10:52 -0300

    Commiting files that should be add in previous commit.

---
+++ client/gmapwrapper-definedarea.js
@@ -0,0 +1,67 @@
+var gLocation;
+
+function initialize() {
+
+  function processResponse(responseText) { 
+  
+      shapeJson = JSON.parse(responseText);
+
+      centerLat = shapeJson.southwest_lat + (Math.sqrt(Math.pow(shapeJson.southwest_lat - shapeJson.northeast_lat, 2))/2);
+      centerLng = shapeJson.southwest_lng + (Math.sqrt(Math.pow(shapeJson.northeast_lng - shapeJson.southwest_lng, 2))/2); 
+
+
+      var mapOptions = {
+        center: new google.maps.LatLng(centerLat, centerLng),
+        // center: new google.maps.LatLng(0, 0),
+        zoom: 13,
+      };
+
+      var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+
+      
+      google.maps.event.addListener(map, 'click', function(event) {
+        placeMarker(event.latLng);
+    });
+
+    function placeMarker(location) {
+        
+        var greenIcon = 'images/greenflag.png'
+        var redIcon = 'images/beachflag.png'
+        
+        if ( ((shapeJson.northeast_lat > location.lat()) && (location.lat()  > shapeJson.southwest_lat)) && 
+             ((shapeJson.northeast_lng > location.lng()) && (location.lng()  > shapeJson.southwest_lng)) ){
+            iconFile = greenIcon;
+        } else {
+            iconFile = redIcon;
+        }
+        
+        console.log(location);
+        gLocation = location;
+        var marker = new google.maps.Marker({
+            position: location, 
+            map: map,
+            icon: iconFile
+    });}
+  };
+    
+  var myUrl = getCookie('servicearea');
+  console.log(myUrl);
+  _SU3.ajax(myUrl, processResponse);  
+    
+  
+}
+
+function getCookie(cname)
+{
+var name = cname + "=";
+var ca = document.cookie.split(';');
+for(var i=0; i<ca.length; i++) 
+  {
+  var c = ca[i].trim();
+  if (c.indexOf(name)==0) return c.substring(name.length+1 ,c.length-1);
+  }
+return "";
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);

99862bad4f02dacc017bd96c5eb31e84ea1253f0
Roberto Allende <rallende@gmail.com>
2014-04-28 21:07:40 -0300

    Fixing variables values for deployment

--- servicearea/development.py
+++ servicearea/development.py
@@ -1,11 +1,17 @@
 '''
 Settings for development environment
 '''
+import os.path
+PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))
+PROJECT_PATH = os.path.abspath(PROJECT_PATH)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = []
+
+''' 
+Using sqlitle, if you need to use mysql uncoment next lines and be sure database is configured as it must.
 
 DATABASES = {
     'default': {
@@ -15,4 +21,17 @@ DATABASES = {
         'PASSWORD': 'saadmin',
     }
 }
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_PATH, 'db.sqlite3'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
 
